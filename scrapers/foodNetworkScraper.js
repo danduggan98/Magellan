ca76ff65d12d888c635ef7a841b886e3599c5d57
danduggan98 <dadu3890@colorado.edu>
2020-01-30 08:55:38 -0700

    Initial puppeteer tests

--- scrapers/foodNetworkScraper.js
+++ scrapers/foodNetworkScraper.js
@@ -1,9 +1,25 @@
 const puppeteer = require('puppeteer');
 
 async function getChefs(){
+    const chefIndex = 'https://www.foodnetwork.com/profiles/talent';
 
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(chefIndex);
+
+    let data = await page.evaluate(() => {
+        const itemName = '.m-PromoList__a-ListItem';
+        const items = document.querySelectorAll(itemName);
+        return Array.from(items).map((item) => {return item.href});
+    });
+
+    var chefList = data;
+    //chefList.push(url);
+
+    console.log(chefList);
+    await browser.close();
 }
 
-async function getRecipes(chefURL){
+async function getRecipes(chefURL){}
 
-}
+getChefs();
