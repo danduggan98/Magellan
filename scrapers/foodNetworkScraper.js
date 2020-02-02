a723eb4899aae3e30a35ab2be99630bef6d9ee88
danduggan98 <dadu3890@colorado.edu>
2020-02-01 22:06:59 -0700

    All functions now async and wrapped in try/catch blocks

--- scrapers/foodNetworkScraper.js
+++ scrapers/foodNetworkScraper.js
@@ -1,48 +1,56 @@
 const puppeteer = require('puppeteer');
 
-//Main function - runs automatically and handles entire process
+//Main function - calls itself automatically and handles entire process
 (async function scrapeSite() {
     try {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
+        const chefList = await getChefs(page); //Store links to all chef pages
 
-        const chefList = await getChefs(browser, page);
-        getRecipes(chefList[2]); //test with Aaron sanchez
-
-        //Store the urls of every recipe from every chef
-        /*for (let i = 0; i < chefList.length; i++){
-            getRecipes(chefList[i]);
+        //Store links to every recipe from every chef
+        for (let i = 0; i < chefList.length; i++){ //Explicit for loop to ensure items are handled in the proper order
+            //getRecipes(chefList[i]);
+            await getRecipes(chefList[i], page);
         }
-        */
 
-        //Shut down the browser on completion
+        //Shut down the browser when finished
         await browser.close();
 
     } catch(err) {
-        console.log("Try/Catch failed:", err);
+        console.log("Error in 'scrapeSite()': ", err);
     }
 })();
 
 //Gather all recipes from the site and store them in a list
-const getChefs = async(browser, page) => {
-    
-    //Get a list of links to every chef
-    await page.goto('https://www.foodnetwork.com/profiles/talent');
+const getChefs = async(page) => {
+    try {
+        //Get a list of links to every chef
+        await page.goto('https://www.foodnetwork.com/profiles/talent');
 
-    //Store the link from each list item in an array
-    const chefList = await page.evaluate(() => {
-        const listItem = '.m-PromoList__a-ListItem > a';
-        const links = document.querySelectorAll(listItem);
-        return Array.from(links).map((link) => {return link.href});
-    });
-    return chefList;
+        //Store the link from each list item in an array
+        const chefList = await page.evaluate(() => {
+            const links = document.querySelectorAll('.m-PromoList__a-ListItem > a');
+            return Array.from(links).map((link) => {return link.href});
+        });
+        return chefList;
+
+    } catch(err) {
+        console.log("Error in 'getChefs': ", err);
+    }
 };
 
-async function getRecipes(chefURL) {
+//Find all recipes from a given chef
+const getRecipes = async(chefURL, page) => {
     //const profileBrowser = await puppeteer.launch();
     //const profilePage = await profileBrowser.newPage();
     //await profilePage.goto(chefURL + '/recipes'); //Can't reach this?
-    console.log(chefURL + '/recipes');
+    try {
+        console.log(chefURL + '/recipes');
+
+    } catch(err) {
+        console.log("Error in 'getRecipes': ", err);
+    }
+
 
     /*let recipeLinks = await page.evaluate(() => {
         const element = '.m-MediaBlock__a-HeadlineText > a';
