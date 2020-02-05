a7f1fe6dbcf0fde2e36b56657d1f88611457cc70
danduggan98 <dadu3890@colorado.edu>
2020-02-04 18:37:12 -0700

    Working mechanism to grab data and store in JSON file

--- scrapers/foodNetworkScraper.js
+++ scrapers/foodNetworkScraper.js
@@ -1,39 +1,29 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const rl = require('readline');
 
-//Implent getData and have main add its contents to a json
+//Export scrapeSite as a module (but keep as self-calling)
+//change recipes.txt to 'FoodNetworkRecipes.txt', repopulate it
 
 //Main function - calls itself automatically and handles entire process
 (async function scrapeSite() {
     try {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.setDefaultNavigationTimeout(0); //Let navigation take as long as it needs
-        const recipeFile = fs.createWriteStream('recipes.txt'); //Write stream for recipe.txt
+        page.setDefaultNavigationTimeout(0); //Let navigation take as long as it needs
 
-        const chefs = await getChefs(page); //List of all chef pages
-
-        //Store links to every recipe from every chef
-        for (let i = 0; i < chefs.length; i++){ //Explicit for loop to ensure items are handled in the proper order
-            console.log('Starting chef', i+1);
-            await getRecipes(chefs[i], page, recipeFile); //Grab all the chef's recipes
-            console.log("Finished chef", i+1);
-        }
-
-        //Save the data from every recipe in a json file (call getData on each item in recipes.txt)
-        /*const curRecipe = "";
-        for (let j = 0; j < masterRecipeList.length; j++){
-            fs.writeFile('recipesData.json', masterRecipeList[j], (err) => { if (err) throw err; });
-            //curRecipe  = await getData(masterRecipeList[j]);
-            //Add all data in list to json file
-        }*/
+        const recipeFileName = 'recipes.txt'; //Name of file which stores our recipe ULRs
+        const JSONFileName = 'FoodNetworkRecipeData.json'; //Name of file which stores our JSON data
         
-        //Shut down the browser when finished
-        recipeFile.end();
+        //Gather all recipes from all chefs
+        //findAllRecipes(page, recipefileName)
+
+        //Convert the recipes file to a JSON
+        await writeRecipesToJSON(page, recipeFileName, JSONFileName);
         await browser.close();
 
     } catch (err) {
-        console.log("Error in 'scrapeSite()':", err);
+        console.log("Error in 'scrapeSite':", err);
     }
 })();
 
@@ -56,7 +46,7 @@ const getChefs = async(page) => {
     }
 };
 
-//Find all recipes from a given chef and add them to a text file
+//Find all recipes from a given chef
 const getRecipes = async(chefURL, page, filestream) => {
     try {
         await page.goto(chefURL + '/recipes'); //Page with all the chef's recipes
@@ -91,8 +81,29 @@ const getRecipes = async(chefURL, page, filestream) => {
     }
 };
 
-//Gather and store all relevant information from a recipe
-const getData = async(recipeURL, page) => {};
+//Gather and store all relevant information from a recipe in JSON format
+const getData = async(recipeURL, page) => {
+    try {
+        await page.goto(recipeURL);
+
+        //Get all relevant data
+        const recipeName = await page.$eval('.o-AssetTitle__a-Headline > span:nth-child(1)', btn => btn.innerText);
+        const totalTime = await page.$eval('div.o-RecipeInfo:nth-child(2) > ul:nth-child(1) > li:nth-child(2) > span:nth-child(2)', btn => btn.innerText);
+
+        //JSON object to store everything
+        const data = {
+            url : recipeURL,
+            recipeName: recipeName,
+            totalTime: totalTime
+            //author: ''
+            //, etc...
+        };
+        return JSON.stringify(data); //Return data in a JSON format
+
+    } catch (err) {
+        console.log("Error in 'getData':", err);
+    }
+};
 
 //Append the contents of an array to a file with a given name
 function writeArrayToFile(array, filestream) {
@@ -102,5 +113,46 @@ function writeArrayToFile(array, filestream) {
         });
     } catch (err) {
         console.log("Error in 'writeArrayToFile':", err);
+    }
+}
+
+//Gather all recipes from all chefs
+async function findAllRecipes(page, fileName){
+    try {
+        const stream = fs.createWriteStream(fileName); //Write stream for recipes list
+        const chefs = await getChefs(page); //List of all chef pages
+
+        //Store links to every recipe from every chef
+        for (let i = 0; i < chefs.length; i++){ //Explicit for loop to ensure items are handled in the proper order
+            console.log('Starting chef', i+1);
+            await getRecipes(chefs[i], page, stream); //Grab all the chef's recipes
+            console.log("Finished chef", i+1);
+        }
+        stream.end();
+
+    } catch (err) {
+        console.log("Error in 'findAllRecipes':", err);
+    }
+}
+
+//Put the data for each recipe into a JSON file
+async function writeRecipesToJSON(page, inFile, outFile) {
+    try {
+        //Prepare the files to be read/written
+        const readStream = fs.createReadStream(inFile);
+        const writeStream = fs.createWriteStream(outFile);
+
+        const lineReader = rl.createInterface({ //Allows us to scan through the file stream line by line
+            input: readStream
+        });
+
+        //Get the data from each url and convert it to JSON
+        for await (const line of lineReader) {
+            const data = await getData(line, page);
+            writeStream.write(data + ',' + '\n'); //Write JSON'd data to the file
+        }
+
+    } catch (err) {
+        console.log("Error in 'readRecipesToJSON':", err);
     }
 }
