9c075f6c814f99052adddf46455dc0a7c8e9e806
danduggan98 <dadu3890@colorado.edu>
2020-03-05 19:42:06 -0700

    Server establishes persistent connection to database

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -1,3 +1,7 @@
+//
+// Creates a shared Mongo database client
+//
+
 //Set up Mongo client
 const MongoClient = require('mongodb').MongoClient;
 const mongoURI = 'mongodb+srv://dduggan:g8RpQ9yiE8Bw@mealscraper-6n2np.mongodb.net/test?retryWrites=true&w=majority&wtimeoutMS=0';
--- server.js
+++ server.js
@@ -1,10 +1,38 @@
+//
+// Web Server
+//
+
 //Set up Express app
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/'));
 
+//Automatically connect to Mongo database, store the connection for reuse
+let database;
+
+(function connectToMongo() {
+    const dbClient = require('./database/dbConnect.js').client;
+    dbClient.connect((err, db) => {
+        if (err) throw err;
+        console.log("Connected to Mongo cluster");
+        database = db; //Store a persistent database connection
+    });
+})();
+
+////////// PAGES \\\\\\\\\\
+
+//Default page - redirects to home
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+
+// Home Page
+app.get('/home', (req, res) => {
+    res.send("WHADUUUUPPPP");
+});
+
 //Server listens on native port, or on 3000 if in a local environment
-var port = process.env.PORT || 3000;
-var server = app.listen(port, () => {
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
     console.log('Magellan server listening on port', port);
 });
