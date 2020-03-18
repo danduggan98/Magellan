713221f3fd562296621e991836f81f65b10148e6
danduggan98 <dadu3890@colorado.edu>
2020-03-17 22:30:01 -0600

    Initial ejs experiments for recipe page, 404+500 errors on server, suppressed console errors

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -5,6 +5,6 @@
 //Set up Mongo client
 const MongoClient = require('mongodb').MongoClient;
 const mongoURI = 'mongodb+srv://dduggan:g8RpQ9yiE8Bw@mealscraper-6n2np.mongodb.net/test?retryWrites=true&w=majority&wtimeoutMS=0';
-const client = new MongoClient(mongoURI, { useNewUrlParser: true });
+const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 module.exports = { client };
--- server.js
+++ server.js
@@ -2,20 +2,29 @@
 // Web Server
 //
 
+//TO-DO
+// Host on Amazon
+// Add search bar functionality
+// Clean + finalize data in Mongo
+// Use Passport for authentication
+// Recipe page
+
 //Set up Express app
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/'));
+app.set('view engine', 'ejs'); //Use EJS for dynamic HTML
 
-//Automatically connect to Mongo database, store the connection for reuse
-let database;
+let database; //Maintains a persistent connection to the Mongo cluster
+const ObjectId = require('mongodb').ObjectID;
 
+//Automatically connect to database, store the connection for reuse
 (function connectToMongo() {
     const dbClient = require('./database/dbConnect.js').client;
     dbClient.connect((err, db) => {
         if (err) throw err;
         console.log("Connected to Mongo cluster");
-        database = db; //Store a persistent database connection
+        database = db; //Save the connection
     });
 })();
 
@@ -29,6 +38,47 @@ app.get('/', (req, res) => {
 // Home Page
 app.get('/home', (req, res) => {
     res.send("WHADUUUUPPPP");
+});
+
+//Load a recipe
+//HANDLE RECIPE NOT FOUND (avoid error 500, instead return not found page)
+app.get('/recipe/:recipeid', (req, res) => {
+    const recipes = database.db('recipeData').collection("recipes"); //Access the recipe list
+
+    //Grab recipe info from database
+    recipes.find(ObjectId(req.params.recipeid)).toArray((err, result) => {
+        if (err) throw err;
+        let recipeData = result[0];
+
+        //Pass each data point to the ejs file
+        res.render('recipe', {
+            URL: recipeData.URL,
+            imageURL: recipeData.imageURL,
+            author: recipeData.author,
+            recipeName: recipeData.recipeName,
+            difficulty: recipeData.difficulty,
+            totalTime: recipeData.totalTime,
+            prepTime: recipeData.prepTime,
+            inactiveTime: recipeData.inactiveTime,
+            activeTime: recipeData.activeTime,
+            cookTime: recipeData.cookTime,
+            yield: recipeData.yield,
+            ingredients: recipeData.ingredients,
+            directions: recipeData.directions,
+            source: recipeData.source
+        });
+    });
+});
+
+//Handle 404 errors
+app.use((req, res) => {
+    res.status(404).send('Error 404 - Page Not Found');
+});
+
+//Handle 500 errors
+app.use((err, req, res, next) => {
+    console.error(err.stack); //Log error details
+    res.status(500).send('Error 500 - Internal Server Error');
 });
 
 //Server listens on native port, or on 3000 if in a local environment
