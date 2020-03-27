2cb30fcc661de508d421b644ca8a78d12da04234
danduggan98 <dadu3890@colorado.edu>
2020-03-26 22:44:21 -0600

    Fixed file paths

--- magellan/src/index.js
+++ magellan/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './App.js';
 
 ReactDOM.render (
-  <App/>,
+  <App />,
   document.getElementById('root')
 );
