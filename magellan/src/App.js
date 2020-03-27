7fb95a1086050132df65f66beaded6436feadd15
danduggan98 <dadu3890@colorado.edu>
2020-03-27 12:08:52 -0600

    Added route to home page

--- magellan/src/App.js
+++ magellan/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Recipe from './components/recipe.js';
+import Home from './components/home.js';
+
 import './styles/App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect
 } from 'react-router-dom';
 
 export default function App() {
@@ -12,6 +15,10 @@ export default function App() {
     <Router>
       <Switch>
         <Route path={'/recipe/:recipeid'} component={Recipe} />
+        <Route path={'/home'} component={Home} />
+        <Route path={'/'}>
+          <Redirect to={'/home'} />
+        </Route>
       </Switch>
     </Router>
   );
--- magellan/src/index.js
+++ magellan/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import App from './App.js';
+import './styles/index.css';
+
 
 ReactDOM.render (
   <App />,
