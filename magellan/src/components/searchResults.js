e11242cc0e2e885a7c636c86d54caf4922294e0f
danduggan98 <dadu3890@colorado.edu>
2020-03-28 21:51:10 -0600

    Added search results component

---
+++ magellan/src/components/searchResults.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class SearchResults extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                <br></br>
+                Search results!
+            </div>
+        );
+    }
+}
+
+export default SearchResults;
