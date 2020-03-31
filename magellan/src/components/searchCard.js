727ec913c07a388c42322295a4940cded843ce2e
danduggan98 <dadu3890@colorado.edu>
2020-03-30 22:36:37 -0600

    Created searchCard component

---
+++ magellan/src/components/searchCard.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class SearchCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>(Search Result)</h4>
+            </div>
+        );
+    }
+}
+
+export default SearchCard;
