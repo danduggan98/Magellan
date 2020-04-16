845c029352f7a9c2dc44959b3abdd2333dc5b5c2
danduggan98 <dadu3890@colorado.edu>
2020-04-16 16:37:46 -0600

    Accepts and prints data passed from the search bar

--- magellan/src/components/searchResults.js
+++ magellan/src/components/searchResults.js
@@ -1,16 +1,24 @@
 import React, {Component} from 'react';
+import SearchCard from './searchCard.js';
 
 class SearchResults extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            results: this.props.data
+        };
     }
 
     render() {
+        const res = Array.from(this.state.results);
+        const list = res.map(result => (
+            <SearchCard info={result} />
+        ));
+
         return (
             <div>
-                <br></br>
-                Search results!
+                <h2>Results:</h2>
+                <h3>{list}</h3>
             </div>
         );
     }
