a5ea6ee77ec014f7dec7cdd5106469d3a73cfe9a
danduggan98 <dadu3890@colorado.edu>
2020-04-16 23:42:18 -0600

    Display a limited number of search results

--- magellan/src/components/searchResults.js
+++ magellan/src/components/searchResults.js
@@ -5,20 +5,33 @@ class SearchResults extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            results: this.props.data
+            results: this.props.data,
+            limit: this.props.lmt
         };
     }
 
     render() {
+        //See if there will be extra recipes for a new page
+        const overflow = (this.state.results.length > this.state.limit);
+        
+        //Grab the recipes we will show, up to the given limit
         const res = Array.from(this.state.results);
-        const list = res.map(result => (
-            <SearchCard info={result} />
+        const visible = res.slice(0, this.state.limit);
+
+        //Turn them into search cards
+        const list = visible.map(recipe => (
+            <SearchCard info={recipe} />
         ));
 
         return (
             <div>
                 <h2>Results:</h2>
                 <h3>{list}</h3>
+
+                { overflow ?
+                    <div>See more results</div>
+                    : <p></p>
+                }
             </div>
         );
     }
