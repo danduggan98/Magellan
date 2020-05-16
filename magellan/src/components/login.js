415d2062bb4feb330b363e083785f89be7a2197d
danduggan98 <dadu3890@colorado.edu>
2020-05-16 10:48:13 -0600

    Visual improvements

--- magellan/src/components/login.js
+++ magellan/src/components/login.js
@@ -5,53 +5,74 @@ class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: "",
-            password: ""
+            usernameInput: '',
+            passwordInput: ''
         };
     }
 
     //Store the most recent inputs in state
     updateInput = (val) => {
-        console.log("WHADDUP");
         let { id, value } = val.target;
-        id = id.toString().slice(0, -5); //usernameInput -> username, passwordInput -> password
         this.setState({ [id]: value });
     }
 
-    submitPage = (val) => {}
+    submitPage = (val) => {
+        if (!this.state.usernameInput) {
+            console.log('PLEASE ENTER USERNAME');
+        }
+        if (!this.state.passwordInput) {
+            console.log('PLEASE ENTER PASSWORD');
+        }
+    }
 
     render() {
         return (
             <div id='loginWrapper'>
-                <div id="loginHeader">Log In</div>
+                <div id='loginHeader'>Log In</div>
                 <form
-                    name='loginForm'>
+                    name='loginForm'
+                    target='hiddenFrame'
+                    onSubmit={this.submitPage}>
 
-                    <div id='usernameInputWrapper'>
-                        <input
-                            name='UN'
-                            id='usernameInput'
-                            type='text'
-                            autoComplete='off'
-                            placeholder='Username'
-                            value={this.state.username}
-                            onChange={this.updateInput}>
-                        </input>
+                    <div id='inputWrapper'>
+                        <div id='usernameInputWrapper'>
+                            <label id='usernameLabel' htmlFor='usernameInput'>Username:</label>
+                            <input
+                                name='UN'
+                                id='usernameInput'
+                                type='text'
+                                autoComplete='off'
+                                placeholder='Username'
+                                value={this.state.username}
+                                onChange={this.updateInput}>
+                            </input>
+                        </div>
+
+                        <div id='passwordInputWrapper'>
+                            <label id='passwordLabel' htmlFor='passwordInput'>Password:</label>
+                            <input
+                                name='PW'
+                                id='passwordInput'
+                                type='password'
+                                autoComplete='off'
+                                placeholder='Password'
+                                value={this.state.password}
+                                onChange={this.updateInput}>
+                            </input>
+                        </div>
                     </div>
 
-                    <div id='passwordInputWrapper'>
-                        <input
-                            name='PW'
-                            id='passwordInput'
-                            type='password'
-                            autoComplete='off'
-                            placeholder='Password'
-                            value={this.state.password}
-                            onChange={this.updateInput}>
-                        </input>
+                    <div id='submitButtonWrapper'>
+                        <button
+                            type='submit'
+                            id='submitButton'>
+                                Submit
+                        </button>
                     </div>
-
                 </form>
+
+                {/* Form redirects to this invisible iframe, keeping it on the same page */}
+                <iframe name='hiddenFrame' id='iframe' title='hidden'></iframe>
             </div>
         );
     }
