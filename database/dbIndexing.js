609aee45191d7268ce5ef4b1e11e7ae88eeb4b83
danduggan98 <dadu3890@colorado.edu>
2020-05-18 23:28:19 -0600

    Trimming results, pt. 1

--- database/dbIndexing.js
+++ database/dbIndexing.js
@@ -28,28 +28,32 @@
                     }
                 });
 
-            //Get all contents from the database
-            console.log('Gathering database terms');
+            //Get all contents from the database, and store all the unique words it contains
+            console.log('Gathering list of unique words to index');
             let searchTerms = [];
+            let recipeList;
 
-            //Go through each recipe's name and ingredients, and store anything that hasn't been seen yet
+            //Gather all the recipes
             recipeDB.collection('recipes').find({}).toArray((err, result) => {
-                let trimmedResult = result.map(element => {
-                    let elName = element.recipeName;
-                    let elIngs = element.ingredients;
+                recipeList = result;
 
+                //Cut the results down to just the recipe name and ingredient list
+                let trimmedResult = recipeList.map(element => {
                     let trimmed = '';
-                    if (elName) trimmed += elName.toString() + ' ';
-                    if (elIngs) trimmed += elIngs.toString();
-
+                    if (element.recipeName) trimmed += element.recipeName.toString() + ' ';
+                    if (element.ingredients) trimmed += element.ingredients.toString();
                     return trimmed;
                 });
 
-                //Remove all numbers, symbols, and useless words
+                //Remove all numbers, symbols, useless words, and extra spaces
                 const numResults = trimmedResult.length;
                 for (let i = 0; i < numResults; i++) {
-                    trimmedResult[i] = trimmedResult[i].replace(/[!@#$%^&*()-+{}:;"'<>,.~`1234567890]+/g, ' ');
-                    trimmedResult[i] = trimmedResult[i].replace(/\[|\]/g, ' ');
+                    trimmedResult[i] = trimmedResult[i].toLowerCase();
+                    trimmedResult[i] = trimmedResult[i].replace(/[!@#$%^&*()-_+{}:;"'<>,.\[\]\/\\\|~`1234567890]+/g, ' ');
+                    trimmedResult[i] = trimmedResult[i].replace(/\s+/g, ' ').trim(); //Take out extra spaces
+
+                    console.log(result[i].recipeName);
+                    console.log(result[i].ingredients);
                     console.log(trimmedResult[i]);
                 }
                 //console.log(trimmedResult);
