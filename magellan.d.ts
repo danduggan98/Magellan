8585db44b19dc7ef70a7a6648acc10c3fe49e30c
danduggan98 <dadu3890@colorado.edu>
2020-05-30 14:29:38 -0600

    Added custom types

---
+++ magellan.d.ts
@@ -0,0 +1,15 @@
+interface recipe {
+    URL: string,
+    imageURL: string,
+    author: string,
+    recipeName: string,
+    difficulty?: string,
+    totalTime?: string,
+    prepTime?: string,
+    inactiveTime?: string,
+    activeTime?: string,
+    cookTime?: string,
+    yield?: string,
+    ingredients: string[],
+    directions: string[]
+}
