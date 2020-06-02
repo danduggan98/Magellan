44d0c0aa57c7c1ae012c9dbc7b7161671f449f98
danduggan98 <dadu3890@colorado.edu>
2020-06-02 13:37:27 -0600

    Added explanations of each type

--- magellan.d.ts
+++ magellan.d.ts
@@ -1,3 +1,8 @@
+//
+// Definitions of all custom types
+//
+
+//Holds each recipe in the format held by the database
 interface RecipeData {
     _id: string,
     URL: string,
@@ -16,23 +21,27 @@ interface RecipeData {
     source?: string
 }
 
+//Information about each JSON file to add recipes from
 interface RecipeDataFile {
     filePath: string,
     source: string
 }
 
+//Compressed version of a recipe used by the indexing script
 interface RecipeDataTrimmed {
     id: string,
     data: string,
     threshold: number
 }
 
+//Format for indexes held by database
 interface Index {
     key: string,
     recipes: IndexReference[],
     frequency: number
 }
 
+//Info about each recipe stored by an index in its 'recipes' array
 interface IndexReference {
     id: string,
     inName: number,
