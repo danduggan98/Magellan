08c78f91627b7fc97ef8b59eb460f9e2887113da
danduggan98 <dadu3890@colorado.edu>
2020-06-03 08:53:22 -0600

    Made custom types indexable

--- magellan.d.ts
+++ magellan.d.ts
@@ -4,46 +4,57 @@
 
 //Holds each recipe in the format held by the database
 export interface RecipeData {
-    _id: string,
-    URL: string,
-    imageURL: string,
-    author: string,
-    recipeName: string,
-    difficulty?: string,
-    totalTime?: string,
-    prepTime?: string,
+    [key: string]: string | string[] | undefined,
+    _id:           string,
+    URL:           string,
+    imageURL:      string,
+    author:        string,
+    recipeName:    string,
+    difficulty?:   string,
+    totalTime?:    string,
+    prepTime?:     string,
     inactiveTime?: string,
-    activeTime?: string,
-    cookTime?: string,
-    yield?: string,
-    ingredients: string[],
-    directions: string[],
-    source?: string
+    activeTime?:   string,
+    cookTime?:     string,
+    yield?:        string,
+    ingredients:   string[],
+    directions:    string[],
+    source?:       string
 }
 
 //Information about each JSON file to add recipes from
 export interface RecipeDataFile {
-    filePath: string,
-    source: string
+    [key: string]: string,
+    filePath:      string,
+    source:        string
 }
 
 //Compressed version of a recipe used by the indexing script
 export interface RecipeDataTrimmed {
-    id: string,
-    data: string,
-    threshold: number
+    [key: string]: string | number,
+    id:            string,
+    data:          string,
+    threshold:     number
 }
 
 //Format for indexes held by database
 export interface Index {
-    key: string,
-    recipes: IndexReference[],
-    frequency: number
+    [key: string]: string | IndexReference[] | number,
+    key:           string,
+    recipes:       IndexReference[],
+    frequency:     number
 }
 
 //Info about each recipe stored by an index in its 'recipes' array
 export interface IndexReference {
-    id: string,
-    inName: number,
-    inIngs: number
+    [key: string]: string | number,
+    id:            string,
+    inName:        number,
+    inIngs:        number
+}
+
+export interface DuplicateRecipe {
+    [key: string]: number,
+    idx:           number,
+    score:         number
 }
