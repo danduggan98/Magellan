edf6f09c5ef6c9bf6d1705d03a6ac7d48c81457c
danduggan98 <dadu3890@colorado.edu>
2020-06-05 13:37:07 -0600

    Fully converted Recipe to TSX

--- src/components/recipe.tsx
+++ src/components/recipe.tsx
@@ -1,36 +1,65 @@
-import React, {Component} from 'react';
+import React, { Component, FunctionComponent } from 'react';
 import { Helmet } from 'react-helmet';
 import '../styles/recipe.css';
+import { RouteComponentProps } from 'react-router-dom';
+import { RecipeData } from 'magellan';
+
+//Define local types
+
+interface ArrayToListProps {
+    list:    string[],
+    ordered: boolean
+}
+
+interface RecipeProps {
+    id: string
+}
+
+interface State {
+    recipeFound:  boolean,
+    recipeID:     string,
+    URL:          string,
+    imageURL:     string,
+    author:       string,
+    recipeName:   string,
+    difficulty:   string | undefined,
+    totalTime:    string | undefined,
+    prepTime:     string | undefined,
+    inactiveTime: string | undefined,
+    activeTime:   string | undefined,
+    cookTime:     string | undefined,
+    yield:        string | undefined,
+    ingredients:  string[],
+    directions:   string[],
+    source:       string | undefined
+}
 
 //Parse an array of ingredients or directions into a JSX list
-function ArrayToList(props) {
-    let items = [];
+const ArrayToList: FunctionComponent<ArrayToListProps> = (props) => {
+    let items: JSX.Element[] = [];
 
     //Iterate through each section
-    const numSections = props.list.length;
-    const ordered = props.ordered;
-
-    for (let i = 0; i < numSections; i++) {
-        let section = [];
+    for (let i = 0; i < props.list.length; i++) {
+        let section: JSX.Element[] = [];
         let itemList = props.list[i];
 
         //Store each item in the inner array as an HTML list item
-        let numItems = itemList.length;
-        let header = itemList[0];
-
-        for (let j = 1; j < numItems; j++) {
+        for (let j = 1; j < itemList.length; j++) { //Start at j = 1 to skip the header
+            let next = itemList[j];
             section.push (
-                <li key={itemList[j].toString()}>
-                    {itemList[j]}
+                <li key={next}>
+                    {next}
                 </li>
             );
         }
 
         //Print the section header if noteworthy
+        const header = itemList[0];
+
         if (header !== 'main') {
             items.push (
                 <div className='sectionHeader'
-                    key={header.toString()}>
+                    key={header}>
                     {header}
                 </div>
             );
@@ -39,12 +68,11 @@ function ArrayToList(props) {
         //Print the list of items
         items.push (
             <div className='sectionData'>
-                { ordered ?
-                    <ol key={section.toString()}>
+                { props.ordered
+                  ? <ol key={section.toString()}>
                         {section}
                     </ol>
-                    :
-                    <ul key={section.toString()}>
+                  : <ul key={section.toString()}>
                         {section}
                     </ul>
                 }
@@ -58,25 +86,37 @@ function ArrayToList(props) {
 }
 
 //Display full recipe data
-class Recipe extends Component {
-    constructor(props) {
+export default class Recipe extends Component<RouteComponentProps<RecipeProps>, State> {
+    constructor(props: RouteComponentProps<RecipeProps>) {
         super(props);
         this.state = {
-            status: 1, //Remains 1 if the recipe is found, 0 otherwise
-            recipeID: props.match.params.recipeid, //URL parameter
-            ingredients: '',
-            directions: ''
+            recipeFound:  true,
+            recipeID:     props.match.params.id, //URL parameter
+            URL:          '',
+            imageURL:     '',
+            author:       '',
+            recipeName:   '',
+            difficulty:   '',
+            totalTime:    '',
+            prepTime:     '',
+            inactiveTime: '',
+            activeTime:   '',
+            cookTime:     '',
+            yield:        '',
+            ingredients:  [],
+            directions:   [],
+            source:       ''
         };
     }
 
     //Gather data from server JSON response
     async componentDidMount() {
         const res = await fetch('/recipe/' + this.state.recipeID);
-        const data = await res.json();
+        const data: RecipeData = await res.json();
 
         //Recipe not found
         if (data.error) {
-            this.setState({ status: 0 });
+            this.setState({ recipeFound: false });
         }
         //Recipe found
         else {
@@ -101,11 +141,11 @@ class Recipe extends Component {
 
     render() {
         //Recipe not found
-        if (!this.state.status) {
+        if (!this.state.recipeFound) {
             return (
-                <div>
-                    <h1>Recipe not found!</h1>
-                    <h3>Please try again</h3>
+                <div id='notFoundNotice'>
+                    Recipe not found!
+                    Please try again
                 </div>
             );
         }
@@ -113,12 +153,11 @@ class Recipe extends Component {
         else {
             return (
                 <div>
-                    { this.state.recipeName ?
-                        <Helmet>
+                    { this.state.recipeName
+                      ? <Helmet>
                             <title>{"Magellan - " + this.state.recipeName}</title>
                         </Helmet>
-                        :
-                        <Helmet>
+                      : <Helmet>
                             <title>{"Magellan"}</title>
                         </Helmet>
                     }
@@ -141,9 +180,9 @@ class Recipe extends Component {
                     </div>
 
                     <div id='image'>
-                        { this.state.imageURL ?
-                            <img src={this.state.imageURL} alt='' width='600'></img>
-                            : <p className='invisibleElement'></p>
+                        { this.state.imageURL
+                          ? <img src={this.state.imageURL} alt='' width='600'></img>
+                          : <p className='invisibleElement'></p>
                         }
 
                         <div id='sourceLink'>
@@ -179,50 +218,50 @@ class Recipe extends Component {
                             <div id='timesList'>
                                 <ul>
                                     <div id='prepTime'>
-                                        { this.state.prepTime ?
-                                            <li>
+                                        { this.state.prepTime
+                                          ? <li>
                                                 <span id='prepTimeText'>
                                                     {this.state.prepTime}
                                                 </span>
                                                 prep time
                                             </li>
-                                            : <p className='invisibleElement'></p>
+                                          : <p className='invisibleElement'></p>
                                         }
                                     </div>
                                     
                                     <div id='cookTime'>
-                                        { this.state.cookTime ?
-                                            <li>
+                                        { this.state.cookTime
+                                          ? <li>
                                                 <span id='cookTimeText'>
                                                     {this.state.cookTime}
                                                 </span>
                                                 cook time
                                             </li>
-                                            : <p className='invisibleElement'></p>
+                                          : <p className='invisibleElement'></p>
                                         }
                                     </div>
                                     
                                     <div id='activeTime'>
-                                        { this.state.activeTime ?
-                                            <li>
+                                        { this.state.activeTime
+                                          ? <li>
                                                 <span id='activeTimeText'>
                                                     {this.state.activeTime}
                                                 </span>
                                                 active time
                                             </li>
-                                            : <p className='invisibleElement'></p>
+                                          : <p className='invisibleElement'></p>
                                         }
                                     </div>
                                     
                                     <div id='inactiveTime'>
-                                        { this.state.inactiveTime ?
-                                            <li>
+                                        { this.state.inactiveTime
+                                          ? <li>
                                                 <span id='inactiveTimeText'>
                                                     {this.state.inactiveTime}
                                                 </span>
                                                 inactive time
                                             </li>
-                                            : <p className='invisibleElement'></p>
+                                          : <p className='invisibleElement'></p>
                                         }
                                     </div>
                                 </ul>
@@ -256,5 +295,3 @@ class Recipe extends Component {
         }
     }
 }
-
-export default Recipe;
