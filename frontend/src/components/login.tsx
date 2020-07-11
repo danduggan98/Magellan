f3148db1e75fd22d7832562e4c406598d7a178e0
danduggan98 <dadu3890@colorado.edu>
2020-07-11 11:04:59 -0600

    Added submitPage function, redirect to homepage on submission

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -18,16 +18,56 @@ export default class Login extends Component {
     };
 
     //Store the most recent inputs in state
-    updateInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    updateInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
         let { id, value } = event.currentTarget;
         this.setState({ [id]: value });
     }
 
-    submitPage = () => {
-        
+    //Submit the form and save any errors that might have returned
+    submitPage = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+
+        const inputs = JSON.stringify({
+            email:    this.state.email,
+            password: this.state.password
+        });
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: inputs
+        };
+
+        try {
+            (async() => {
+                const response = await fetch('/auth/login', options);
+                const errors: string[] = await response.json();
+                
+                if (!errors.length) {
+                    this.setState({
+                        redirectAfterSumbit: true
+                    });
+                }
+                else {
+                    this.setState({
+                        errors
+                    });
+                }
+            })();
+        }
+        catch (err) {
+            console.log('Error submitting login form:', err);
+        }
     }
 
     render() {
+        if (this.state.redirectAfterSumbit) {
+            return (<Redirect to='/home' />);
+        }
+
         return (
             <div id='loginWrapper'>
                 <div id='loginHeader'>Log In</div>
@@ -88,7 +128,7 @@ export default class Login extends Component {
                         </button>
                     </div>
                 </form>
-                
+
             </div>
         );
     }
