164c99cdf1514e6b0e60e69a8e6f6cd58b829752
danduggan98 <dadu3890@colorado.edu>
2020-07-11 20:41:49 -0600

    Check that token exists before trying to verify

--- backend/middleware/validateRequest.ts
+++ backend/middleware/validateRequest.ts
@@ -3,18 +3,22 @@ import { Request, Response } from 'express';
 
 //Middleware to validate JSON web tokens in requests
 // Add to any api route to make it private
-export default function verifyJWT(req: Request, res: Response) {
+export default function verifyJWT(req: Request, res: Response): Response | void {
     try {
-        const token = req.header('auth-token') || '';
+        const token = req.header('auth-token');
+        if (!token) {
+            return res.status(401).json({
+                auth_error: 'Access denied - You must log in to reach this page'
+            });
+        }
+
         const secret = process.env.JWT_SECRET || '';
         const validToken = jwt.verify(token, secret);
 
-        if (token && validToken && req.session) {
-            req.user = validToken; //Store the email from the token in our request
-        }
+        if (validToken) req.user = validToken; //Store the email from the token in our request
         else {
             return res.status(401).json({
-                error: 'Access denied - You must log in to reach this page'
+                auth_error: 'Access denied - invalid token'
             });
         }
     }
