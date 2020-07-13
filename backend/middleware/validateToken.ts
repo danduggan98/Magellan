167cc5bc43e50b81c50a97b868d29add54344275
danduggan98 <dadu3890@colorado.edu>
2020-07-13 07:33:24 -0600

    Now returns a 'false' flag when verification fails

--- backend/middleware/validateToken.ts
+++ backend/middleware/validateToken.ts
@@ -1,13 +1,17 @@
 import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 
-//Middleware to validate JSON web tokens in requests
-// Add to any api route to make it private
+//Middleware to validate JSON web tokens from API requests
+// Add this to any API route to make it private/require login
+// On failure, sends a 'false' flag and an error message
+// On success, stores the user email in the request and moves to the next function
+
 export default function verifyJWT(req: Request, res: Response, next: NextFunction): Response | void {
     try {
         const token = req.header('auth-token');
         if (!token) {
             return res.status(401).json({
+                verified: false,
                 auth_error: 'Access denied - You must log in to reach this page'
             });
         }
@@ -15,14 +19,16 @@ export default function verifyJWT(req: Request, res: Response, next: NextFunctio
         const secret = process.env.JWT_SECRET || '';
         const validToken = jwt.verify(token, secret);
 
-        if (validToken) {
-            req.user = validToken; //Store the email from the token in our request
-            next();
-        }
-        else {
+        if (!validToken) {
             return res.status(401).json({
+                verified: false,
                 auth_error: 'Access denied - invalid token'
             });
+            
+        }
+        else {
+            req.user = validToken; //Store the email from the token in our request
+            next();
         }
     }
     catch (err) {
