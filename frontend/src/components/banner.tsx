ccf2203e5d5ece2c6a86f5e9cf2b4322ad34960b
danduggan98 <dadu3890@colorado.edu>
2020-07-12 20:43:22 -0600

    Now checks login status on load

--- frontend/src/components/banner.tsx
+++ frontend/src/components/banner.tsx
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/banner.css'
 
+interface State {
+    verified: boolean
+};
+
 export default class Banner extends Component {
+    state: State = {
+        verified: false
+    };
+
+    async componentDidMount() {
+        const response = await fetch('/auth/verified');
+        const authCheck = await response.json();
+
+        this.setState({
+            verified: authCheck.verified
+        });
+    }
+
     render() {
         return (
             <div id='logoBanner'>
                 <Link to='/home' className='logo'>
                     MAGELLAN
                 </Link>
+
+                <div>VERIFIED? {this.state.verified.toString()}</div>
 
                 <Link to='/login' className='loginButton'>
                     Log In
