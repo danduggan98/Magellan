3df4ca9cb13dd9f31fd27fed72e5f11627927d93
danduggan98 <dadu3890@colorado.edu>
2020-07-19 20:47:55 -0600

    Made searchCard component fully controlled

--- frontend/src/components/searchCard.tsx
+++ frontend/src/components/searchCard.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { FunctionComponent } from 'react';
 import { RecipeDataResult } from '../../../magellan';
 import '../styles/searchCard.css';
 
@@ -6,73 +6,62 @@ interface Props {
     info: RecipeDataResult
 }
 
-interface State {
-    recipe: RecipeDataResult
-}
+const SearchCard: FunctionComponent<Props> = (props) => {
+    const data = props.info;
+    const link = `/recipe/${data._id}`;
 
-export default class SearchCard extends Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            recipe: props.info
-        };
+    //Select the appropriate class so we can give each source a unique color
+    let src = '';
+        
+    switch(data.source) {
+        case ("Food Network"):
+            src = "FNsource";
+            break;
+        case ("Taste of Home"):
+            src = "TOHsource";
+            break;
     }
 
-    render() {
-        const data = this.state.recipe;
-        const link = `/recipe/${data._id}`;
+    return (
+        <div id='card' className='expandableCard'>
+            <a className='cardRecipeLink'
+                target='_blank'
+                rel='noopener noreferrer'
+                href={link}>
 
-        //Select the appropriate class so we can give each source a unique color
-        let src = '';
-        
-        switch(data.source) {
-            case ("Food Network"):
-                src = "FNsource";
-                break;
-            case ("Taste of Home"):
-                src = "TOHsource";
-                break;
-        }
+                <div id='cardContents'>
+                    <div id='cardInfo'>
+                        <div id='cardRecipeName'>{data.recipeName}</div>
+                        <div id='secondaryDetails'>
+                            { data.author
+                                ? <div id='cardAuthor'>by {data.author}</div>
+                                : <p></p>
+                            }
 
-        return (
-            <div id='card' className='expandableCard'>
-                <a className='cardRecipeLink'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    href={link}>
-
-                    <div id='cardContents'>
-                        <div id='cardInfo'>
-                            <div id='cardRecipeName'>{data.recipeName}</div>
-                            <div id='secondaryDetails'>
-                                { data.author
-                                    ? <div id='cardAuthor'>by {data.author}</div>
+                            <div id='tertiaryDetails'>
+                                { data.source
+                                    ? <div id='cardSource' className={src}>
+                                        from {data.source}
+                                      </div>
                                     : <p></p>
                                 }
 
-                                <div id='tertiaryDetails'>
-                                    { data.source
-                                        ? <div id='cardSource' className={src}>
-                                            from {data.source}
-                                          </div>
-                                        : <p></p>
-                                    }
-
-                                    { data.totalTime
-                                        ? <div id='cardTotalTime'>{data.totalTime}</div>
-                                        : <p></p>
-                                    }
-                                    
-                                </div>
+                                { data.totalTime
+                                    ? <div id='cardTotalTime'>{data.totalTime}</div>
+                                    : <p></p>
+                                }
+                                
                             </div>
                         </div>
-                        { data.imageURL
-                            ? <img id='cardPhoto' src={data.imageURL} alt=''></img>
-                            : <p></p>
-                        }
                     </div>
-                </a>
-            </div>
-        );
-    }
+                    { data.imageURL
+                        ? <img id='cardPhoto' src={data.imageURL} alt=''></img>
+                        : <p></p>
+                    }
+                </div>
+            </a>
+        </div>
+    );
 }
+
+export default SearchCard;
