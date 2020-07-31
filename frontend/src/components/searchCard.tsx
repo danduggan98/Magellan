de435c726107892360b2b6eb4fce852cdc2319a0
danduggan98 <dadu3890@colorado.edu>
2020-07-31 16:21:26 -0600

    Condensed card to just the title and time

--- frontend/src/components/searchCard.tsx
+++ frontend/src/components/searchCard.tsx
@@ -10,54 +10,28 @@ const SearchCard: FunctionComponent<Props> = (props) => {
     const data = props.info;
     const link = `/recipe/${data._id}`;
 
-    //Select the appropriate class so we can give each source a unique color
-    let src = '';
-        
-    switch(data.source) {
-        case ("Food Network"):
-            src = "FNsource";
-            break;
-        case ("Taste of Home"):
-            src = "TOHsource";
-            break;
-    }
-
     return (
-        <div id='card' className='expandableCard'>
+        <div id='card'>
             <a className='cardRecipeLink'
                 target='_blank'
                 rel='noopener noreferrer'
                 href={link}>
 
                 <div id='cardContents'>
+                    <div id='cardPhoto'>
+                        { data.imageURL
+                          ? <img src={data.imageURL} alt=''></img>
+                          : <p className='invisibleElement'></p>
+                        }
+                    </div>
+                    
                     <div id='cardInfo'>
                         <div id='cardRecipeName'>{data.recipeName}</div>
-                        <div id='secondaryDetails'>
-                            { data.author
-                              ? <div id='cardAuthor'>by {data.author}</div>
-                              : <p></p>
-                            }
-
-                            <div id='tertiaryDetails'>
-                                { data.source
-                                  ? <div id='cardSource' className={src}>
-                                        from {data.source}
-                                    </div>
-                                  : <p></p>
-                                }
-
-                                { data.totalTime
-                                    ? <div id='cardTotalTime'>{data.totalTime}</div>
-                                    : <p></p>
-                                }
-                                
-                            </div>
-                        </div>
+                        { data.totalTime
+                            ? <div id='cardTotalTime'>{data.totalTime}</div>
+                            : <p className='invisibleElement'></p>
+                        }
                     </div>
-                    { data.imageURL
-                      ? <img id='cardPhoto' src={data.imageURL} alt=''></img>
-                      : <p></p>
-                    }
                 </div>
             </a>
         </div>
