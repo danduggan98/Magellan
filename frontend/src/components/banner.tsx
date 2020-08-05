7ff9ab15c796a54b408f68f1f2b893bc802da9dc
danduggan98 <dadu3890@colorado.edu>
2020-08-04 21:21:00 -0600

    Rearranged banner to include all useful links simultaneously

--- frontend/src/components/banner.tsx
+++ frontend/src/components/banner.tsx
@@ -11,36 +11,41 @@ interface Props extends RouteComponentProps {
 const Banner: FunctionComponent<Props> = (props) => {
     return (
         <div id='banner'>
-            <Link to='/home' className='logo'>
-                Magellan
-            </Link>
-
-            { props.location.pathname === '/home'
-              ? props.verified
-                ? <Link to='/user'
-                    className='linkButton'>
-                        <span className='linkButtonText'>View Your Saved Recipes</span>
-                  </Link>
-                : <p className='invisibleElement'></p>
-              : <Link to='/home'
-                    className='linkButton'>
-                        <span className='linkButtonText'>Back to the home page</span>
+            <div id='bannerLeft'>
+                <Link to='/home' className='logo'>
+                    Magellan
                 </Link>
-            }
 
-            { props.verified
-              ? <button className='linkButton' onClick={props.logout}>
-                    <span className='linkButtonText'>Sign Out</span>
-                </button>
-              : <Link
-                    to={{
-                        pathname: '/login',
-                        state: { source: props.location.pathname }
-                    }}
-                    className='linkButton'>
-                        <span className='linkButtonText'>Sign In</span>
-                </Link>
-            }
+                { props.location.pathname === '/home'
+                  ? <p className='invisibleElement'></p>
+                  : <Link to='/home'
+                        className='linkButton'>
+                            <span className='linkButtonText'>Back to the home page</span>
+                    </Link>
+                }
+            </div>
+            
+            <div id='bannerRight'>
+                { props.verified
+                  ? <div>
+                        <Link to='/user'
+                            className='linkButton'>
+                                <span id='userLinkButtonText' className='linkButtonText'>View Your Saved Recipes</span>
+                        </Link>
+                        <button className='linkButton' onClick={props.logout}>
+                            <span className='linkButtonText'>Sign Out</span>
+                        </button>
+                    </div>
+                  : <Link
+                        to={{
+                            pathname: '/login',
+                            state: { source: props.location.pathname }
+                        }}
+                        className='linkButton'>
+                            <span className='linkButtonText'>Sign In</span>
+                    </Link>
+                }
+            </div>
         </div>
     );
 }
